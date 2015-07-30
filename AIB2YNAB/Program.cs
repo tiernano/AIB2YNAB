03b0eb6144a8a3e7664e58b2cfb88c8ecbe154d8
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-07-30 09:30:50 +0100

    initial checkin

---
+++ AIB2YNAB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIB2YNAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Count() == 2)
+            {
+                Console.WriteLine("Expecting 2 Parameters: Input File and Output File");
+            }
+        }
+    }
+}
