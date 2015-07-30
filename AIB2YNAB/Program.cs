25754744b0f86ef2c0fd4edcbd77ed97429f7294
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-07-30 10:28:47 +0100

    now using csv helper for reading csv file

--- AIB2YNAB/Program.cs
+++ AIB2YNAB/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -31,28 +32,40 @@ namespace AIB2YNAB
                     Console.WriteLine("WARN: Output exists. Will be overwritten");
                     File.Delete(outputFile);
                 }
-                string[] lines = File.ReadAllLines(inputFile);
+
+
                 StringBuilder outputLines = new StringBuilder();
 
                 outputLines.AppendLine("Date,Payee,Category,Memo,Outflow,Inflow");
-
-                foreach(string line in lines.Skip(1))
+                var csv = new CsvReader(File.OpenText(inputFile));
+                while (csv.Read())
                 {
-                    if (string.IsNullOrWhiteSpace(line))
-                    {
-                        continue;
-                    }
-                    string[] parts = line.Split(',');
-                    string date = parts[1];
-                    string fixedDate = DateTime.ParseExact(date.Replace('\"',' ').Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
-                    string desc = parts[2];
-                    string debit = parts[3];
-                    string credit = parts[4];
+                    string date = csv.GetField<string>(1);
+                    string fixedDate = DateTime.ParseExact(date.Replace('\"', ' ').Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
+                    string desc = csv.GetField<string>(2);
+                    string debit = csv.GetField<string>(3);
+                    string credit = csv.GetField<string>(4);
+
+                    debit = cleanNumber(debit);
+
+                    credit = cleanNumber(credit);
+
                     outputLines.AppendLine(string.Format("{0},{1},,,{2},{3}", fixedDate, desc, debit, credit));
                 }
+               
 
                 File.WriteAllText(outputFile, outputLines.ToString());
             }
+        }
+
+        private static string cleanNumber(string input)
+        {
+            string output = input;
+            while(output.IndexOf(',') > -1)
+            {
+                output = output.Remove(output.IndexOf(','), 1);
+            }
+            return output;
         }
     }
 }
